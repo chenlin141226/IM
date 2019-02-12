1870bb1e4deafb44c37f58cfa8ebc4966ed7097b
Jaffa <affa@ecoph.tk>
2019-02-12 16:53:26 +0800

    重新整合后提交

--- app/src/main/java/com/jaffa/hardy/im/SplashActivity.kt
+++ app/src/main/java/com/jaffa/hardy/im/SplashActivity.kt
@@ -1,17 +1,40 @@
 package com.jaffa.hardy.im
 
+import android.os.Handler
 import com.jaffa.hardy.im.contract.SplashContract
+import com.jaffa.hardy.im.presenter.SplashActivityPresenter
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : BaseActivity(),SplashContract.View {
 
     override fun getlayoutResId(): Int = R.layout.activity_splash
 
-    //登录后UI的处理
-    override fun onLoggedIn() {
+    val presenter = SplashActivityPresenter(this)
+
+    val handler by lazy { Handler() }
+
+    companion object {
+       const val DELAY = 2000L
     }
 
-    //没有登录UI的处理
+    init {
+        presenter.checkLoginStatus()
+    }
+
+
+    //登录后UI的处理
     override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)
+    }
+
+
+    //没有登录UI的处理
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
     }
 
 }
