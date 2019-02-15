596281f5e13e0286defaf6e2758d7c545605c099
Jaffa <affa@ecoph.tk>
2019-02-15 17:42:35 +0800

    联系人列表条目布局，联系人RecycleView的初始化

---
+++ app/src/main/java/com/jaffa/hardy/im/adapter/ContactListAdapter.kt
@@ -0,0 +1,28 @@
+package com.jaffa.hardy.im.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jaffa.hardy.im.R
+import java.text.FieldPosition
+
+class ContactListAdapter(val context : Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(patent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.view_contact_item,patent,false)
+
+        return ContactListItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = 30
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+    class ContactListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
--- app/src/main/java/com/jaffa/hardy/im/ui/fragment/ContactFragment.kt
+++ app/src/main/java/com/jaffa/hardy/im/ui/fragment/ContactFragment.kt
@@ -1,6 +1,12 @@
 package com.jaffa.hardy.im.ui.fragment
 
+import android.graphics.Color
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.jaffa.hardy.im.R
+import com.jaffa.hardy.im.adapter.ContactListAdapter
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.header.*
 
 class ContactFragment : BaseFragment(){
     
@@ -8,5 +14,20 @@ class ContactFragment : BaseFragment(){
 
     override fun init() {
         super.init()
+        headerTitle.text = getString(R.string.contact)
+        add.visibility = View.VISIBLE
+
+        //apply写法
+        swipeRefreshLayout.apply {
+            setColorSchemeColors(Color.BLUE)
+            isRefreshing = true
+        }
+
+        recyclerView.apply {
+            //当条目数据发生变化时，会优化
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = ContactListAdapter(context)
+        }
     }
 }
